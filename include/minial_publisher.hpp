042b6cfb50f7f8397bc7d1e073c6705da0ef584e
Naveen Mangla <nmangla@umd.edu>
2022-11-30 12:46:54 -0500

    Licence

--- include/minial_publisher.hpp
+++ include/minial_publisher.hpp
@@ -1,3 +1,28 @@
+/*
+Copyright 2022 Naveen Mangla
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ * @file minial_publisher.hpp
+ * @author your name (you@domain.com)
+ * @brief Publisher class
+ * @version 0.1
+ * @date 2022-11-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <chrono>
 #include <memory>
 #include "geometry_msgs/msg/transform_stamped.hpp"
